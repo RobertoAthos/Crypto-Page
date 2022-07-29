7c16c026cc7098931ab1f79589973149f8679c28
Roberto Athos <robertoathos22@gmail.com>
2022-07-29 11:22:59 -0300

    done

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-
+let connect =  document.querySelector('#connect')
 //Metamaskt verification
 if(typeof window.ethereum !== 'undefined'){
 
-    document.querySelector('#connect').addEventListener('click', async()=>{
+   if(connect){
+    connect.addEventListener('click', async()=>{
         try {
             const accounts = await ethereum.request({method:'eth_requestAccounts'})
             const account = accounts[0]
             document.querySelector('#account').innerHTML = account
-
             //Redirect to home after connected metamask
             setTimeout(()=> {
-                window.location.href = "./cryptoPage.html";
-            }, 2000);
+                let congrats = document.querySelector('.congrats')
+                congrats.style.display = "block"
+                document.querySelector('.congrats-btn').addEventListener('click', ()=>{
+                    congrats.style.display = 'none'
+                })
+            }, 2000); 
 
         } catch(error){
             console.log(error)
         }
     })
+   }
 
     
 }else{
     let error = document.querySelector(".error")
     error.style.display = 'block'
-    document.querySelector('#close-btn').addEventListener('click', ()=>{
+    document.querySelector('.close-btn').addEventListener('click', ()=>{
         error.style.display = 'none'
     })
 }
